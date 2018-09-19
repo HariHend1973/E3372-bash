4aa9fd74d68eccaa2b4273c662c18edcaec1b94c
Hari Hendaryanto <hari.hendaryanto@gmail.com>
2018-09-19 17:15:53 +0700

    E3372 reboot

---
+++ e3372-reboot.sh
@@ -0,0 +1,9 @@
+#!/bin/sh 
+
+MODEM_IP="192.168.9.1" 
+curl -s -X GET "http://$MODEM_IP/api/webserver/SesTokInfo" > ses_tok.xml
+COOKIE=`grep "SessionID=" ses_tok.xml | cut -b 10-147`
+TOKEN=`grep "TokInfo" ses_tok.xml | cut -b 10-41` 
+
+curl -s -X POST "http://$MODEM_IP/api/device/control" -H "Cookie: $COOKIE" -H "__RequestVerificationToken: $TOKEN" -H "Content-Type: text/xml" -d "<?xml version="1.0" encoding="UTF-8"?><request><Control>1</Control></request>"
+
