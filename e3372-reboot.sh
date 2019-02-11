8f90106ba1f1dcb23086f8c7a31e46143e615f77
Hari Hendaryanto <hari.hendaryanto@gmail.com>
2019-02-11 15:12:25 +0700

    test visual studio code

--- e3372-reboot.sh
+++ e3372-reboot.sh
@@ -7,3 +7,4 @@ TOKEN=`grep "TokInfo" ses_tok.xml | cut -b 10-41`
 
 curl -s -X POST "http://$MODEM_IP/api/device/control" -H "Cookie: $COOKIE" -H "__RequestVerificationToken: $TOKEN" -H "Content-Type: text/xml" -d "<?xml version="1.0" encoding="UTF-8"?><request><Control>1</Control></request>"
 
+#test
